eb5d1eb026129b5573f1e04c29ba9e7b67094da2
GYANAM-DEVELOPER <74140267+GYANAM-ui@users.noreply.github.com>
2021-10-29 09:49:14 +0530

    Update main.py

--- spyware/main.py
+++ spyware/main.py
@@ -2,7 +2,7 @@ import requests
 import numpy as np
 import cv2
 while True:
-    images=requests.get("http://192.168.43.69:8080/shot.jpg")
+    images=requests.get("http://:8080/shot.jpg")
     video=np.array(bytearray(images.content),dtype=np.uint8)
     rendor=cv2.imdecode(video,-1)
 
